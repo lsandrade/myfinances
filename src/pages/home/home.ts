73d2b94c15fceea027ee2178144ef1e54e9a8839
Luan Andrade <luansandrade92@hotmail.com>
2017-02-22 16:54:42 -0300

    ngfor, ngif e oninit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 import { NavController } from 'ionic-angular';
 
@@ -9,15 +9,25 @@ import {ServiceProvider} from '../../providers/service-provider';
   selector: 'page-home',
   templateUrl: 'home.html'
 })
-export class HomePage {
+export class HomePage implements OnInit{
 
   teste: string = "Teste";
-
   users:any[];
+  nome: boolean = false;
 
-  constructor(public navCtrl: NavController, public service: ServiceProvider) {
+  constructor(public navCtrl: NavController, public service: ServiceProvider) {}
+
+  ngOnInit(){
     this.getDados();
   }
+
+  mostraNome(){
+    if(this.nome)
+      this.nome = false;
+    else
+      this.nome = true;
+  }
+
   getDados(){
     this.service.getData().subscribe(
       data => this.users = data.users,
